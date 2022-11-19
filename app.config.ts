88faad1527126742e5f6883d2361c66639e2b874
clemcode <clement.o2p@gmail.com>
2022-11-19 16:57:29 +0100

    5 files modified, 4 added, 5 deleted

---
+++ app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Clément Bourseau',
+    description: 'Modélisation 3D',
+    header: {
+      position: 'inline', // possible value are : 'none' | 'left' | 'center' | 'right' | 'inline'
+      logo: false,
+    },
+    socials: {
+      github: '',
+      twitter: '',
+    },
+  },
+});
